e897b19b889adbf58ca40cf439857874dc959872
BENJA <be.cortesp@duocuc.cl>
2024-08-19 15:37:31 -0400

    commit_2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './page/home/home.component';
+import { ContactoComponent } from './page/contacto/contacto.component';
+import { QuienesSomosComponent } from './page/quienes-somos/quienes-somos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactoComponent,
+    QuienesSomosComponent
   ],
   imports: [
     BrowserModule,
